53ff0cd9f49298f72125bb7c41fa8042b84b7f44
frankenbeans <Emmanuel.Lamprecht@gmail.com>
2024-11-10 00:31:36 +0200

    Add files via upload

---
+++ SOURCE/sfrealcomp.f95
@@ -0,0 +1,59 @@
+! ------------------------------------------------------------------------------
+! SFREALCOMP.F95
+!
+! Practical real-number comparisons for SFDEFLIC project
+!
+! Copyright (C) 2023 by E. Lamprecht - all rights reserved
+! ------------------------------------------------------------------------------
+
+MODULE SFRealComp
+  USE Constant, ONLY: RKIND
+  IMPLICIT NONE
+
+CONTAINS
+
+  PURE FUNCTION REQ(R1,R2)
+    ! -- TRUE if R1 ~= R2, FALSE otherwise --
+    IMPLICIT NONE
+    LOGICAL :: REQ
+    REAL(KIND=RKIND),INTENT(IN) :: R1,R2
+    ! --- EXE CODE ---
+    REQ=(ABS(R1-R2).LE.TINY(R1))
+    ! --- END CODE ---
+  END FUNCTION REQ
+
+  PURE FUNCTION RNE(R1,R2)
+    ! -- TRUE if R1 not ~= R2, FALSE otherwise --
+    IMPLICIT NONE
+    LOGICAL :: RNE
+    REAL(KIND=RKIND),INTENT(IN) :: R1,R2
+    ! --- EXE CODE ---
+    RNE=(ABS(R1-R2).GT.TINY(R1))
+    ! --- END CODE ---
+  END FUNCTION RNE
+
+  ELEMENTAL FUNCTION RIZ(R1)
+    ! -- TRUE if R1 ~= 0, FALSE otherwise --
+    IMPLICIT NONE
+    LOGICAL :: RIZ
+    REAL(KIND=RKIND),INTENT(IN) :: R1
+    ! --- EXE CODE ---
+    RIZ=ABS(R1).LE.TINY(R1)
+    ! --- END CODE ---
+  END FUNCTION RIZ
+
+  ELEMENTAL FUNCTION RNZ(R1)
+    ! -- TRUE if R1 not ~= 0, FALSE otherwise --
+    IMPLICIT NONE
+    LOGICAL :: RNZ
+    REAL(KIND=RKIND),INTENT(IN) :: R1
+    ! --- EXE CODE ---
+    RNZ=ABS(R1).GT.TINY(R1)
+    ! --- END CODE ---
+  END FUNCTION RNZ
+  
+END MODULE SFRealComp
+  
+  
+  
+  

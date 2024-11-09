7bbb717f2086314eb53975bd5e4d986a7d579570
frankenbeans <Emmanuel.Lamprecht@gmail.com>
2024-11-10 00:23:25 +0200

    Add files via upload

---
+++ SOURCE/caseconv.f
@@ -0,0 +1,62 @@
+* ----------------------------------------------------------------------
+* CASECONV.F
+*
+* FORTRAN-77 ROUTINES FOR CONVERTING STRINGS TO UPPERCASE OR LOWERCASE.
+*
+* COPYRIGHT (C) 2014 BY E. LAMPRECHT.  ALL RIGHTS RESERVED.
+* ----------------------------------------------------------------------
+
+* $CVSHeader: MZ2/SOURCE/caseconv.f,v 1.1.1.1 2021/05/23 02:02:44 emman Exp $
+
+* ----------------------------------------------------------------------
+* SUBROUTINE TOUPPR(C)
+* --------------------
+* CONVERT VARIABLE-LENGTH CHARACTER STRING C TO UPPER CASE.
+* 
+      SUBROUTINE TOUPPR(C)
+      IMPLICIT NONE
+      CHARACTER*(*) C
+      EXTERNAL LTRSHF
+      CALL LTRSHF(C,.TRUE.)
+      END
+*     SUBROUTINE TOUPPR
+* ----------------------------------------------------------------------
+* SUBROUTINE TOLOWR(C)
+* --------------------
+* CONVERT VARIABLE-LENGTH CHARACTER STRING C TO LOWER CASE.
+* 
+      SUBROUTINE TOLOWR(C)
+      IMPLICIT NONE
+      CHARACTER*(*) C
+      EXTERNAL LTRSHF
+      CALL LTRSHF(C,.FALSE.)
+      END
+*     SUBROUTINE TOLOWR
+* ----------------------------------------------------------------------
+* SUBROUTINE LTRSHF(C,TOUP)
+* -------------------------
+* SHIFT SINGLE-LETTER CHARACTER STRING C TO UPPER CASE IF LOGICAL TOUP
+* CONTAINS THE VALUE .TRUE., OR TO LOWER CASE OTHERWISE.
+*
+* NOTE:  ONLY LETTERS 'A-z' ARE AFFECTED;  ALL OTHER CHARACTERS ARE
+* ----   IGNORED.
+*
+      SUBROUTINE LTRSHF(C,TOUP)
+      IMPLICIT NONE
+      CHARACTER*(*) C
+      LOGICAL       TOUP
+      INTEGER       SHFT,J
+      LOGICAL       TOSHFT
+      SHFT=ICHAR('a')-ICHAR('A')
+      IF (TOUP) SHFT=-1*SHFT
+      DO 10 J=1,LEN(C)
+         TOSHFT=(TOUP       .AND.ICHAR(C(J:J)).GE.ICHAR('a')
+     $                      .AND.ICHAR(C(J:J)).LE.ICHAR('z'))
+     $          .OR.
+     $          (.NOT.TOUP  .AND.ICHAR(C(J:J)).GE.ICHAR('A')
+     $                      .AND.ICHAR(C(J:J)).LE.ICHAR('Z'))
+         IF (TOSHFT) C(J:J)=CHAR(ICHAR(C(J:J))+SHFT)
+ 10   CONTINUE
+      END
+*     SUBROUTINE LTRSHF
+* ----------------------------------------------------------------------
---
+++ SOURCE/logist.f
@@ -0,0 +1,74 @@
+* ----------------------------------------------------------------------
+*     LOGIST.F
+*
+*     LOGISTIC FUNCTION (SIGMOID CURVE)
+*
+*     COPYRIGHT (C) 2021 BY E. LAMPRECHT.  ALL RIGHTS RESERVED.
+* ----------------------------------------------------------------------
+*
+      FUNCTION LOGIST(X)
+      IMPLICIT NONE
+      DOUBLE PRECISION LOGIST
+*     --- DUMMY ARG ---
+      DOUBLE PRECISION X
+*     --- EXE CODE  ---
+      LOGIST=1.0D0 / (1.0D0 + EXP(-X))
+*     --- END CODE  ---
+      END
+* ======================================================================
+      SUBROUTINE LTDATA(ADATA,ADIML,ADIMR,LXVAL,RXVAL)
+      IMPLICIT NONE
+*     --- DUMMY ARG ---
+      DOUBLE PRECISION ADATA,LXVAL,RXVAL
+      INTEGER          ADIML,ADIMR
+      DIMENSION ADATA(ADIML:ADIMR)
+*     --- VARIABLES ---
+      INTEGER          J
+      DOUBLE PRECISION D2XM,D2XC
+*     --- EXTERNAL ---
+      DOUBLE PRECISION LOGIST
+*     --- EXE CODE ---
+      IF (.NOT.ADIML.LT.ADIMR) GOTO 900
+      IF (.NOT.LXVAL.LT.RXVAL) GOTO 910
+      D2XM=(RXVAL-LXVAL)/(DBLE(ADIMR)-DBLE(ADIML))
+      D2XC=LXVAL-D2XM*DBLE(ADIML)
+      DO 10 J=ADIML,ADIMR
+         ADATA(J)=LOGIST(D2XM*DBLE(J)+D2XC)
+ 10   CONTINUE
+*     --- END CODE ---
+      GOTO 999
+ 800  FORMAT('! ERROR(LTDATA):',1X,A)
+ 900  WRITE(*,800) 'EXPECTING ADIML.LT.ADIMR'
+      STOP
+ 910  WRITE(*,800) 'EXPECTING LXVAL.LT.RXVAL'
+      STOP
+ 999  END
+* ======================================================================
+      SUBROUTINE LTSCAL(ADATAO,ADATAI,ADIML,ADIMR,YVAL0,YVAL1)
+      IMPLICIT NONE
+*     --- DUMMY ARG ---
+      DOUBLE PRECISION ADATAO,ADATAI,YVAL0,YVAL1
+      INTEGER          ADIML, ADIMR
+      DIMENSION ADATAO(ADIML:ADIMR),ADATAI(ADIML:ADIMR)
+*     --- VARIABLES ---
+      INTEGER J
+      DOUBLE PRECISION SCLM,SCLC
+*     --- EXE CODE  ---
+      IF (.NOT.ADATAI(ADIML).LT.ADATAI(ADIMR)) GOTO 900
+      SCLM=(YVAL1-YVAL0) / (ADATAI(ADIMR)-ADATAI(ADIML))
+      SCLC=YVAL0-SCLM*ADATAI(ADIML)
+*     $OMP PARALLEL DO
+      DO 10 J=ADIML,ADIMR
+         ADATAO(J)=SCLM*ADATAI(J)+SCLC
+ 10   CONTINUE
+*     $OMP END PARALLEL DO
+*     --- END CODE  ---
+      GOTO 999
+ 800  FORMAT('! ERROR(LTSCAL):',1X,A)
+ 900  WRITE(*,800) 'EXPECTING ADATAI(ADIML).LT.ADATAI(ADIMR)'
+      STOP      
+ 999  END
+* ======================================================================
+      
+     
+      
